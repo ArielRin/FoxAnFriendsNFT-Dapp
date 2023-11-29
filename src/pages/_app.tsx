614d7df4820dc76b00fa8059762192790240103b
Cheyne Hayworth <degenftm@gmail.com>
2023-11-29 16:43:02 +1100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,17 +23,10 @@ const chains = [
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "";
 
 const metadata = {
-	<meta name="description" content="Explore the thrilling world of Defi Derby, the number 1 raceday AMA on BSC. Join the revolution in decentralized finance and experience a new era of racing and community engagement.  #DefiDerby #BSC #DeFi" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="icon" href="/favicon.ico" />
-
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:site" content="@lastm3042" />
-	<meta name="twitter:title" content="Defi Derby - #1 Raceday AMA on BSC" />
-	<meta name="twitter:description" content="Explore the thrilling world of Defi Derby, the number 1 raceday AMA on BSC. Join the revolution in decentralized finance and experience a new era of racing and community engagement. #DefiDerby #BSC #DeFi" />
-	<meta name="twitter:image" content="https://monumental-donut-cd05a8.netlify.app/twitter.jpg" /> {/* Add the path to your image or logo */}
-	<meta name="twitter:image:alt" content="Defi Derby Logo" />
-
+	name: "Next Starter Template",
+	description: "A Next.js starter template with Web3Modal v3 + Wagmi",
+	url: "https://web3modal.com",
+	icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
 const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
